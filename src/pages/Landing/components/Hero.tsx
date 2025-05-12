9e5ad071bce8f8a120f4d601349755bd7d9413a7
jasonLapina <lapina.jason@gmail.com>
2025-05-12 19:46:34 +0800

    hero style changes

--- src/pages/Landing/components/Hero.tsx
+++ src/pages/Landing/components/Hero.tsx
@@ -1,34 +1,17 @@
-import { Box, Container, useTheme } from "@mui/material";
+import { Container, Stack } from "@mui/material";
+import HeroImg from "./HeroImg.tsx";
+import HeroText from "./HeroText.tsx";
+import HeroCta from "./HeroCta.tsx";
 
 const Hero = () => {
-  const theme = useTheme();
-
   return (
-    <Box
-      sx={{
-        background: `linear-gradient(135deg, ${theme.palette.primary.dark}, ${theme.palette.primary.main}, ${theme.palette.secondary.main})`,
-        color: "white",
-        py: { xs: 8, md: 12 },
-        mb: 8,
-        borderRadius: "20px",
-        boxShadow: "0 10px 30px rgba(0, 0, 0, 0.15)",
-        position: "relative",
-        overflow: "hidden",
-        "&::before": {
-          content: '""',
-          position: "absolute",
-          top: 0,
-          left: 0,
-          right: 0,
-          bottom: 0,
-          background:
-            "radial-gradient(circle at top right, rgba(255,255,255,0.2) 0%, transparent 70%)",
-          zIndex: 1,
-        },
-      }}
-    >
-      <Container maxWidth="lg"></Container>
-    </Box>
+    <Container maxWidth="lg" sx={{ my: 8 }}>
+      <Stack useFlexGap alignItems={"center"}>
+        <HeroImg />
+        <HeroText />
+        <HeroCta />
+      </Stack>
+    </Container>
   );
 };
 
--- src/pages/Landing/components/HeroImg.tsx
+++ src/pages/Landing/components/HeroImg.tsx
@@ -1,22 +1,45 @@
-import { Box } from "@mui/material";
+import { Box, Stack } from "@mui/material";
+
+const ADJUSTMENT_VAL = "40px";
+
+const IMG_MAX_HEIGHT = 320;
 
 const HeroImg = () => {
   return (
-    <Box
-      component="img"
-      src="/images/landing-light.jpg"
-      alt="Latest tech devices"
-      sx={{
-        width: "100%",
-        height: "auto",
-        maxHeight: 400,
-        objectFit: "cover",
-        display: "block",
-        mx: "auto",
-        borderRadius: 2,
-        boxShadow: "0px 10px 15px rgba(0, 0, 0, 0.2)",
-      }}
-    />
+    <Stack direction={"row"} sx={{ mb: 8 }}>
+      <Box
+        component="img"
+        src="/images/landing-dark.jpg"
+        alt="Latest tech devices"
+        sx={{
+          width: "100%",
+          height: "auto",
+          maxHeight: IMG_MAX_HEIGHT,
+          objectFit: "cover",
+          display: "block",
+          mx: "auto",
+          borderRadius: 2,
+          boxShadow: "0px 10px 15px rgba(0, 0, 0, 0.2)",
+          transform: `translate(${ADJUSTMENT_VAL},-${ADJUSTMENT_VAL})`,
+        }}
+      />
+      <Box
+        component="img"
+        src="/images/landing-light.jpg"
+        alt="Latest tech devices"
+        sx={{
+          width: "100%",
+          height: "auto",
+          maxHeight: IMG_MAX_HEIGHT,
+          objectFit: "cover",
+          display: "block",
+          mx: "auto",
+          borderRadius: 2,
+          boxShadow: "0px 10px 15px rgba(0, 0, 0, 0.2)",
+          transform: `translate(-${ADJUSTMENT_VAL},${ADJUSTMENT_VAL})`,
+        }}
+      />
+    </Stack>
   );
 };
 
